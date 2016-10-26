1b1bc7f94f7b624e15cb6939935319ea0b9262c0
Drew Kowalski <drewkowalski93@gmail.com>
2016-10-25 23:32:09 -0400

    Committing example of a possible organization with sample rest call

--- TravisScripts/install.sh
+++ TravisScripts/install.sh
@@ -5,8 +5,8 @@
 echo 'Downloading Pre-reqs: '
 pip install -r ./TacticsServer/requirements.txt
 
-echo 'Downloading from http://netstorage.unity3d.com/unity/b7e030c65c9b/MacEditorInstaller/Unity-5.4.2f2.pkg: '
-curl -o Unity.pkg http://netstorage.unity3d.com/unity/b7e030c65c9b/MacEditorInstaller/Unity-5.4.2f2.pkg
+#echo 'Downloading from http://netstorage.unity3d.com/unity/b7e030c65c9b/MacEditorInstaller/Unity-5.4.2f2.pkg: '
+#curl -o Unity.pkg http://netstorage.unity3d.com/unity/b7e030c65c9b/MacEditorInstaller/Unity-5.4.2f2.pkg
 
-echo 'Installing Unity.pkg'
-sudo installer -dumplog -package Unity.pkg -target /
+#echo 'Installing Unity.pkg'
+#sudo installer -dumplog -package Unity.pkg -target /
