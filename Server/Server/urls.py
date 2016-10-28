20ab51123a5189d9a244ac4955ef32647021fe56
Drew Kowalski <drewkowalski93@gmail.com>
2016-10-28 18:38:16 -0400

    renamed TacticsServer to Server

--- Server/Server/urls.py
+++ Server/Server/urls.py
@@ -1,4 +1,4 @@
-"""TacticsServer URL Configuration
+"""Server URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.10/topics/http/urls/
--- TravisScripts/install.sh
+++ TravisScripts/install.sh
@@ -3,9 +3,9 @@
 # This link changes from time to time. I haven't found a reliable hosted installer package for doing regular
 # installs like this. You will probably need to grab a current link from: http://unity3d.com/get-unity/download/archive
 echo 'Downloading Pre-reqs: '
-pip install -r ./TacticsServer/requirements.txt
+pip install -r ./Server/requirements.txt
 
-python ./TacticsServer/manage.py runserver
+python ./Server/manage.py runserver
 #echo 'Downloading from http://netstorage.unity3d.com/unity/b7e030c65c9b/MacEditorInstaller/Unity-5.4.2f2.pkg: '
 #curl -o Unity.pkg http://netstorage.unity3d.com/unity/b7e030c65c9b/MacEditorInstaller/Unity-5.4.2f2.pkg
 
