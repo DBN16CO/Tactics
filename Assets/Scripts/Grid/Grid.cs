7ceaf4a22c182d20d6af0484487b557ae061f837
Nick Priore <nr.priore@gmail.com>
2016-10-27 22:10:36 -0400

    added grid / modified grass

--- Assets/Scripts/Grid/Grid.cs
+++ Assets/Scripts/Grid/Grid.cs
@@ -1,12 +1,37 @@
 ﻿using UnityEngine;
 
-public class Grid {
+public class Grid : MonoBehaviour{
+
+	private Token[][] _tokens;
+
+
+#region Setters and Getters
+	// Returns _tokens
+	public Token[][] Tokens {
+		get{return _tokens;}
+		set{_tokens = value;}
+	}
+#endregion
+
 
 	// Runs when a new grid is instantiated
-	public Grid(int size) {
-		for(int length = 0; length < size; length++) {
-			for(int width = 0; width < size; width++) {
+	public void CreateGrid(int size) {
+		// Create Tokens jagged array
+		Tokens = new Token[size][];
+		for(int x = 0; x < size; x++) {
+			Tokens[x] = new Token[size];
+		}
 
+		float orthoSize = Camera.main.orthographicSize;
+		float tokenScale = (2f * orthoSize) / (float)size;
+		for(int width = 0; width < size; width++) {
+			for(int height = 0; height < size; height++) {
+				Token token = (Instantiate(Resources.Load("Prefabs/Token"),new Vector2(((float)width*tokenScale)-orthoSize,((float)height*tokenScale)-orthoSize),Quaternion.identity) as GameObject).GetComponent<Token>();
+				token.gameObject.transform.localScale = new Vector3(tokenScale,tokenScale,1);
+				token.SetTerrain("Grass");
+				Tokens[width][height] = token;
+				token.X = width;
+				token.Y = height;
 			}
 		}
 	}
