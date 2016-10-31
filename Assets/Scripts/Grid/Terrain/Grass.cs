eaa7538cfe1143c8d8e47a86880d9df599c71b65
Nick Priore <nr.priore@gmail.com>
2016-10-31 02:53:16 -0400

    more testing

--- Assets/Scripts/Grid/Terrain/Grass.cs
+++ Assets/Scripts/Grid/Terrain/Grass.cs
@@ -7,7 +7,7 @@ public class Grass : TokenTerrain {
 	// Pass in token so we can set the sprite image
 	public Grass(GameObject token) {
 		// Name is used to generate asset path so make sure it is the sprite name in the resources folder
-		string spriteName = "Grass10";
+		string spriteName = "Grass33";
 		Name = token.name = spriteName;
 		// Calls parent function to set token's sprite image
 		SetSprite(token);
