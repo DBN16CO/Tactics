ab85eadfefd13474c3a9ef8a99feda53c956e9b7
Drew Kowalski <drewkowalski93@gmail.com>
2016-11-01 18:41:40 -0400

    added Communication class for unity frontend

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,10 +1,43 @@
 ﻿using UnityEngine;
+using System.Threading;
+using System;
 
 public class GameController : MonoBehaviour {
 
 	void Start() {
 		Grid grid = gameObject.AddComponent<Grid>();
 		grid.CreateGrid(8);
+
+
+		//Example sending and receiving data synchronously
+		Communication.Connect(new Uri("ws://localhost:8000"));
+		Communication.SendString ("{\"PING\":\"PING\"}");
+		string response = null;
+		while (response == null) {
+			response = Communication.RecvString ();
+		}
+
+		Debug.Log ("Received: " + response);		
+
+
+		//Example sending and receiving data asynchronously and doing something with the response
+		new Thread(GameController.PingServerPrintResponse).Start();
+
+		Debug.Log("Continuing on...");
+		
+
+	}
+
+	public static void PingServerPrintResponse(){
+		Communication.Connect(new Uri("ws://localhost:8000"));
+		Communication.SendString("{\"PING\":\"PING\"}");
+
+		string response = null;
+		while (response == null) {
+			response = Communication.RecvString ();
+		}
+
+		Debug.Log ("Received in pingServerPrintResponse: " + response);
 	}
 
 }
