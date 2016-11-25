127fa1cf626f039af342232fabc50d142a3d3a44
Drew Kowalski <drewkowalski93@gmail.com>
2016-11-25 15:24:01 -0500

    Added ability to recursively parse dictionaries and to parse json arrays

--- Assets/Scripts/Communication/Json.cs
+++ Assets/Scripts/Communication/Json.cs
@@ -1,57 +1,73 @@
 ﻿using System.Collections.Generic;
 using Newtonsoft.Json;
+
+using System.Linq;
 using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Converters;
+using System;
+
+/*
+	Custom Dictionary Converter used to deserialize Json Objects to a Dictionary<string, object> type
+*/
+class DictConverter : CustomCreationConverter<IDictionary<string, object>>
+{
+    public override IDictionary<string, object> Create(Type objectType)
+    {
+        return new Dictionary<string, object>();
+    }
+
+    public override bool CanConvert(Type objectType)
+    {
+        return objectType == typeof(object) || base.CanConvert(objectType);
+    }
+
+    public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+    {
+        if (reader.TokenType == JsonToken.StartObject
+            || reader.TokenType == JsonToken.Null)
+            return base.ReadJson(reader, objectType, existingValue, serializer);
+
+        return serializer.Deserialize(reader);
+    }
+}
+
+
+/*
+	Custom List Converter used to deserialize Json Arrays to a List<object> type
+*/
+class ListConverter : CustomCreationConverter<IList<object>>
+{
+    public override IList<object> Create(Type objectType){
+    	return new List<object>();
+    }
+
+    public override bool CanConvert(Type objectType)
+    {
+        return objectType == typeof(List<object>) || base.CanConvert(objectType);
+    }
+
+    public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+    {
+        if (reader.TokenType == JsonToken.Null || reader.TokenType == JsonToken.StartArray)
+            return base.ReadJson(reader, objectType, existingValue, serializer);
+
+        // if the next token is not an object
+        // then fall back on standard deserializer (strings, numbers etc.)
+        return serializer.Deserialize(reader);
+    }
+}
 
 public static class Json {
-
-	/*
-		Example Usage:
-
-		var request = new Dictionary<string, object>();
-		request["PING"] = "PING";
-
-		//Example sending and receiving data synchronously
-		Communication.Connect(new Uri("ws://localhost:8000"));
-		Communication.SendString (Json.ToString(request));
-		string response = null;
-		while (response == null) {
-			response = Communication.RecvString ();
-		}
-
-		Debug.Log ("Received: " + response);
-		Debug.Log("Response toDict: " + Json.ToDict(response)["PONG"]);
-
-		//Example sending and receiving data asynchronously and doing something with the response
-		new Thread(GameController.PingServerPrintResponse).Start();
-
-		Debug.Log("Continuing on...");
-
-		public static void PingServerPrintResponse(){
-			var request = new Dictionary<string, object>();
-			request["PING"] = "PING";
-
-			Communication.Connect(new Uri("ws://localhost:8000"));
-			Communication.SendString(Json.ToString(request));
-
-			string response = null;
-			while (response == null) {
-				response = Communication.RecvString ();
-			}
-
-			Debug.Log ("Received in pingServerPrintResponse: " + response);
-
-			Debug.Log("Response in pingServerPrintResponse - toDict: " + Json.ToDict(response)["PONG"]);
-		}
-
-
-	*/
 
 	public static string ToString(Dictionary<string, object> map){
         return JsonConvert.SerializeObject(map);
 	}
 
-
 	public static Dictionary<string, object> ToDict(string json){
-		return JsonConvert.DeserializeObject<Dictionary<string, object>>(json);
+		return JsonConvert.DeserializeObject<Dictionary<string, object>>(json, new JsonConverter[] {new DictConverter()});
+	}
+
+	public static List<object> ToList(string json){
+		return JsonConvert.DeserializeObject<List<object>>(json, new JsonConverter[] {new ListConverter()});
 	}
 }
