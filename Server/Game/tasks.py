b32269405f3888c2b0ad338b8081d8154f8474ce
Drew Kowalski <drewkowalski93@gmail.com>
2016-12-16 14:03:40 -0500

    Finished the basic matchmaking logic

--- Server/Game/tasks.py
+++ Server/Game/tasks.py
@@ -4,7 +4,7 @@ from celery import shared_task
 import celery
 import datetime
 import random
-from Game.models import Game, Game_Queue, Game_User
+from Game.models import Game, Game_Queue, Game_User, Unit
 from Static.models import Version
 
 import logging
@@ -18,7 +18,7 @@ logger.setLevel(logging.DEBUG)
 def processMatchmakingQueue(mock_queue=None, mock_version=None, mock_maps=None, mock_game_users=None):
 
     #logger = processMatchmakingQueue.get_logger(logfile='./matchmaking.log')
-    logger.info("PROCESSING MATCHMAKING QUEUE")
+    logger.info("Processing matchmaking queue")
 
     # Get the matchmaking queue
     if not mock_queue:
@@ -34,6 +34,7 @@ def processMatchmakingQueue(mock_queue=None, mock_version=None, mock_maps=None, 
 
     # If there isn't at least 2 players in the queue then exit
     if len(queue) < 2:
+        logger.info("Not enough people in the queue to match...Exiting")
         return
 
     # Take the first player of the queue
@@ -43,13 +44,31 @@ def processMatchmakingQueue(mock_queue=None, mock_version=None, mock_maps=None, 
     index = random.randint(1, len(queue) - 1)
     second_player = queue[index]
 
-    player_turn_index = random.randint(1, 2)
+    logger.info("Found a match: " + str(first_player.user.username) + " vs. " + str(second_player.user.username))
+
+    # Determine the number of times these 2 players have played against each other
+    count = 0
+    for first_player_game_user in Game_User.objects.filter(user=first_player.user):
+        game_played = first_player_game_user.game
+
+        if not game_played:
+            continue
+
+        is_second_player_in_game = Game_User.objects.filter(user=second_player.user, game=game_played).first()
+
+        if is_second_player_in_game:
+            count += 1
+
+    logger.info("Users have played each other " + str(count) + " times.")
 
     # Randomly choose which player will start first
+    player_turn_index = random.randint(1, 2)
     player_turn = None
     if player_turn_index == 1:
+        logger.info(str(first_player.user.username) + " will take the first turn")
         player_turn = first_player.user
     elif player_turn_index == 2:
+        logger.info(str(second_player.user.username) + " will take the first turn")
         player_turn = second_player.user
 
     if not mock_maps:
@@ -61,24 +80,45 @@ def processMatchmakingQueue(mock_queue=None, mock_version=None, mock_maps=None, 
     index = random.randint(0, maps.count() - 1)
     game_map = maps.filter()[index]
 
+    logger.info("Map chosen for this game: " + str(game_map))
+
     # Create the game
     game = Game(user_turn=player_turn, version=version, map_path=game_map)
     game.save()
+
+    logger.info("Created the game: " + str(game))
 
     if not mock_game_users:
         game_users = Game_User.objects
     else:
         game_users = mock_game_users
 
+    logger.info("Updating game_user entries")
+
     # Link the game to the first player's game entry
     game_user1 = game_users.filter(user=first_player.user, game=None).first()
     game_user1.game = game
+    game_user1.name = "vs. " + str(second_player.user.username) + " #" + str(count + 1)
     game_user1.save()
 
     # Link the game to the second player's game entry
     game_user2 = game_users.filter(user=second_player.user, game=None).first()
     game_user2.game = game
+    game_user2.name = "vs. " + str(first_player.user.username) + " #" + str(count + 1)
     game_user2.save()
+
+    logging.info("Updating each player's unit entries")
+
+    # Update each player's units
+    for unit in Unit.objects.filter(owner=first_player.user, game=None):
+        unit.game = game
+        unit.save()
+
+    for unit in Unit.objects.filter(owner=second_player.user, game=None):
+        unit.game = game
+        unit.save()
+
+    logger.info("Deleting players from the queue")
 
     # Delete the first player from the queue
     first_player.delete()
