0a95abefb13e63a1cafadef95c7920c5aaa0a024
Drew Kowalski <drewkowalski93@gmail.com>
2016-12-30 14:55:34 -0500

    adding function comment to matchmaking function

--- Server/Game/tasks.py
+++ Server/Game/tasks.py
@@ -15,101 +15,111 @@ logger.setLevel(logging.DEBUG)
 
 @celery.decorators.periodic_task(run_every=datetime.timedelta(seconds=5))
 def processMatchmakingQueue():
+    """
+    This is the entry point for the matchmaking logic. Celery workers will periodically execute
+    this logic and match players that were placed in the game queue. All the data needed to setup a game
+    will happen here.
 
-    logger.debug("Processing matchmaking queue")
+    The logging output for the matchmaking logic will be stored in matchmaking.log
+    """
+    try:
+        logger.debug("Processing matchmaking queue")
 
-    # Get the matchmaking queue
-    queue = Game_Queue.objects.filter()
+        # Get the matchmaking queue
+        queue = Game_Queue.objects.filter()
 
-    # Get the latest version to use for creating games
-    version = Version.objects.latest('pk')
+        # Get the latest version to use for creating games
+        version = Version.objects.latest('pk')
 
-    # If there isn't at least 2 players in the queue then exit
-    if len(queue) < 2:
-        logger.debug("Not enough people in the queue to match...Exiting")
-        return
+        # If there isn't at least 2 players in the queue then exit
+        if len(queue) < 2:
+            logger.debug("Not enough people in the queue to match...Exiting")
+            return
 
-    # Take the first player of the queue
-    first_player = queue[0]
+        # Take the first player of the queue
+        first_player = queue[0]
 
-    # Randomly choose a second player from the remaining players on the queue
-    index = random.randint(1, len(queue) - 1)
-    second_player = queue[index]
+        # Randomly choose a second player from the remaining players on the queue
+        index = random.randint(1, len(queue) - 1)
+        second_player = queue[index]
 
-    logger.info("Found a match: " + str(first_player.user.username) + " vs. " + str(second_player.user.username))
+        logger.info("Found a match: " + str(first_player.user.username) + " vs. " + str(second_player.user.username))
 
-    # Determine the number of times these 2 players have played against each other
-    count = 0
-    for first_player_game_user in Game_User.objects.filter(user=first_player.user):
-        game_played = first_player_game_user.game
+        # Determine the number of times these 2 players have played against each other
+        count = 0
+        for first_player_game_user in Game_User.objects.filter(user=first_player.user):
+            game_played = first_player_game_user.game
 
-        if not game_played:
-            continue
+            if not game_played:
+                continue
 
-        is_second_player_in_game = Game_User.objects.filter(user=second_player.user, game=game_played).first()
+            is_second_player_in_game = Game_User.objects.filter(user=second_player.user, game=game_played).first()
 
-        if is_second_player_in_game:
-            count += 1
+            if is_second_player_in_game:
+                count += 1
 
-    logger.info("Users have played each other " + str(count) + " times.")
+        logger.info("Users have played each other " + str(count) + " times.")
 
-    # Randomly choose which player will start first
-    player_turn_index = random.randint(1, 2)
-    player_turn = None
-    if player_turn_index == 1:
-        logger.info(str(first_player.user.username) + " will take the first turn")
-        player_turn = first_player.user
-    elif player_turn_index == 2:
-        logger.info(str(second_player.user.username) + " will take the first turn")
-        player_turn = second_player.user
+        # Randomly choose which player will start first
+        player_turn_index = random.randint(1, 2)
+        player_turn = None
+        if player_turn_index == 1:
+            logger.info(str(first_player.user.username) + " will take the first turn")
+            player_turn = first_player.user
+        elif player_turn_index == 2:
+            logger.info(str(second_player.user.username) + " will take the first turn")
+            player_turn = second_player.user
 
-    maps = Map.objects
+        maps = Map.objects
 
-    # Randomly choose a map for the game
-    index = random.randint(0, maps.count() - 1)
-    game_map = maps.filter()[index]
+        # Randomly choose a map for the game
+        index = random.randint(0, maps.count() - 1)
+        game_map = maps.filter()[index]
 
-    logger.info("Map chosen for this game: " + str(game_map))
+        logger.info("Map chosen for this game: " + str(game_map))
 
-    # Create the game
-    game = Game(user_turn=player_turn, version=version, map_path=game_map)
-    game.save()
+        # Create the game
+        game = Game(user_turn=player_turn, version=version, map_path=game_map)
+        game.save()
 
-    logger.info("Created the game: " + str(game))
+        logger.info("Created game id: " + str(game.id))
 
-    game_users = Game_User.objects
+        game_users = Game_User.objects
 
-    logger.debug("Updating game_user entries")
+        logger.debug("Updating game_user entries")
 
-    # Link the game to the first player's game entry
-    game_user1 = game_users.filter(user=first_player.user, game=None).first()
-    game_user1.game = game
-    game_user1.name = "vs. " + str(second_player.user.username) + " #" + str(count + 1)
-    game_user1.team = 1
-    game_user1.save()
+        # Link the game to the first player's game entry
+        game_user1 = game_users.filter(user=first_player.user, game=None).first()
+        game_user1.game = game
+        game_user1.name = "vs. " + str(second_player.user.username) + " #" + str(count + 1)
+        game_user1.team = 1
+        game_user1.save()
 
-    # Link the game to the second player's game entry
-    game_user2 = game_users.filter(user=second_player.user, game=None).first()
-    game_user2.game = game
-    game_user2.name = "vs. " + str(first_player.user.username) + " #" + str(count + 1)
-    game_user2.team = 2
-    game_user2.save()
+        # Link the game to the second player's game entry
+        game_user2 = game_users.filter(user=second_player.user, game=None).first()
+        game_user2.game = game
+        game_user2.name = "vs. " + str(first_player.user.username) + " #" + str(count + 1)
+        game_user2.team = 2
+        game_user2.save()
 
-    logging.debug("Updating each player's unit entries")
+        logging.debug("Updating each player's unit entries")
 
-    # Update each player's units
-    for unit in Unit.objects.filter(owner=first_player.user, game=None):
-        unit.game = game
-        unit.save()
+        # Update each player's units
+        for unit in Unit.objects.filter(owner=first_player.user, game=None):
+            unit.game = game
+            unit.save()
 
-    for unit in Unit.objects.filter(owner=second_player.user, game=None):
-        unit.game = game
-        unit.save()
+        for unit in Unit.objects.filter(owner=second_player.user, game=None):
+            unit.game = game
+            unit.save()
 
-    logger.debug("Deleting players from the queue")
+        logger.debug("Deleting players from the queue")
 
-    # Delete the first player from the queue
-    first_player.delete()
+        # Delete the first player from the queue
+        first_player.delete()
 
-    # Delete the second player from the queue
-    second_player.delete()
+        # Delete the second player from the queue
+        second_player.delete()
+    except Exception, e:
+        logger.error("Exception in the matchmaking logic")
+        logger.exception(e)
