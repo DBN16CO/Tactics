e7c96ee277c3facebec0307e9832eb250240b232
Nick Priore <nr.priore@gmail.com>
2017-01-16 12:30:07 -0500

    detailed game view mechanics

---
+++ Assets/Scripts/ActiveCustomGameController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ActiveCustomGameController : MonoBehaviour {
+
+	private MainMenuController _mc;
+
+	public Button btn;
+	private GameObject _detailedView;
+
+	private bool _expanding;			// When detailedview is expanding
+	private bool _expanded;				// When detailedview is expanded
+	public bool _collapsing;			// When detailedview is collapsing
+	private float _t;					// Arbitrary measure of time
+	private RectTransform _detailedViewRT;
+	private float _deltaY;
+
+	// Use this for initialization
+	void Awake () {
+		_mc = GameObject.Find("MainMenuController").GetComponent<MainMenuController>();
+		gameObject.name = gameObject.name.Substring(0, 16);
+
+		_expanding = false;
+		_expanded = false;
+		_collapsing = false;
+		_t = 0f;
+
+		btn.onClick.AddListener(ToggleDetailedView);
+		_detailedView = gameObject.transform.FindChild("DetailedView").gameObject;
+	}
+
+	public void SetDetailedProperties(int index) {
+		_detailedView.name += (index + 1).ToString();
+		btn.transform.FindChild("Text").GetComponent<Text>().text += (index + 1).ToString();
+
+		_detailedViewRT = _detailedView.GetComponent<RectTransform>();
+	}
+
+	void ToggleDetailedView() {
+		_t = 0f;
+		if(_expanding || _expanded) {
+			_expanding = false;
+			_collapsing = true;
+		}else{
+			_collapsing = false;
+			_expanding = true;
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(_t >= 1f) {
+			_expanded = _expanding;
+			_expanding = false;
+			_collapsing = false;
+			_t = 0f;
+		}else if(_expanding || _collapsing) {
+			_t += Time.deltaTime;
+			float preY = _detailedViewRT.anchoredPosition.y;
+			_detailedViewRT.anchoredPosition = new Vector3(0,Mathf.Lerp(_detailedViewRT.anchoredPosition.y,Convert.ToInt32(_expanding) * -500,_t));
+			_deltaY = _detailedViewRT.anchoredPosition.y - preY;
+			LerpBelowGames();
+		}
+	}
+
+	private void LerpBelowGames() {
+		int currIndex = transform.GetSiblingIndex();
+		int maxY = (transform.parent.childCount - 1) * -300;
+		for(int i = currIndex-1; i >= 0; i--) {
+			RectTransform childRT = transform.parent.GetChild(i).GetComponent<RectTransform>();
+			childRT.anchoredPosition = new Vector2(0,childRT.anchoredPosition.y + _deltaY);
+		}
+		RectTransform parentRT = transform.parent.GetComponent<RectTransform>();
+		parentRT.sizeDelta = new Vector2(1440,parentRT.sizeDelta.y - _deltaY);
+	}
+
+}
--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -1,5 +1,4 @@
 ﻿using UnityEngine;
-using UnityEngine.UI;
 
 public class MainMenuController : MonoBehaviour {
 
@@ -8,10 +7,7 @@ public class MainMenuController : MonoBehaviour {
 	private float _ttab;				// Arbitrary measure of time for tab movement
 	private const float _ttabmax = 0.2f;// Unit of time it takes to move the selected tab
 	private float _ttabmult;			// Multiplier calculated to make _ttabmax the time it takes
-
-	// References to the transforms of the moving objects
 	private RectTransform _selectedTabRT;
-
 
 	// The real value of time to use for lerp functions
 	private float _timetab {
@@ -23,8 +19,23 @@ public class MainMenuController : MonoBehaviour {
 		_moving = false;
 		_ttab = 0f;
 		_ttabmult = 1f / _ttabmax;
-
 		_selectedTabRT = (RectTransform)GameObject.Find("SelectedTab").transform;
+
+		LoadCustomGames();
+	}
+
+	private void LoadCustomGames() {
+		// Run the QGU command here. For now, just populate dummy games
+		int numGames = 8;
+		Transform activeGames = GameObject.Find("ActiveCustomGamesContent").transform;
+		for(int i = 0; i < numGames; i++) {
+			GameObject currGame = Instantiate(Resources.Load("Prefabs/ActiveCustomGame"), Vector3.zero, Quaternion.identity, activeGames) as GameObject;
+			currGame.GetComponent<ActiveCustomGameController>().SetDetailedProperties(i);
+
+			currGame.GetComponent<RectTransform>().anchoredPosition = new Vector3(0,-300 * i);
+			currGame.transform.SetAsFirstSibling();
+		}
+		activeGames.GetComponent<RectTransform>().sizeDelta = new Vector3(1440,300 * numGames);
 	}
 
 	void Update () {
