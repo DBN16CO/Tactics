a90c48e40ab1900c0ba8559230d9492f0a164280
Nick Priore <nr.priore@gmail.com>
2017-01-16 19:36:20 -0500

    comments

--- Assets/Scripts/ActiveCustomGameController.cs
+++ Assets/Scripts/ActiveCustomGameController.cs
@@ -13,10 +13,10 @@ public class ActiveCustomGameController : MonoBehaviour {
 	private bool _expanded;				// When detailedview is expanded
 	public bool _collapsing;			// When detailedview is collapsing
 	private float _t;					// Arbitrary measure of time
-	private RectTransform _detailedViewRT;
-	private float _deltaY;
+	private RectTransform _detailedViewRT; // Transform of the detailedview
+	private float _deltaY;				// The amount the detailedview lerped THIS frame
 
-	// Use this for initialization
+	// Initialization - set variables & add click listener
 	void Awake () {
 		_mc = GameObject.Find("MainMenuController").GetComponent<MainMenuController>();
 		gameObject.name = gameObject.name.Substring(0, 16);
@@ -30,13 +30,16 @@ public class ActiveCustomGameController : MonoBehaviour {
 		_detailedView = gameObject.transform.FindChild("DetailedView").gameObject;
 	}
 
+	// Since can't pass params into Awake constructor, set detailed properties after instantiation
 	public void SetDetailedProperties(int index) {
+		// Below code is for testing - makes the dummy games say Game1, Game2 etc
 		_detailedView.name += (index + 1).ToString();
 		btn.transform.FindChild("Text").GetComponent<Text>().text += (index + 1).ToString();
 
 		_detailedViewRT = _detailedView.GetComponent<RectTransform>();
 	}
 
+	// Toggles whether to collapse or expand the detailedview
 	void ToggleDetailedView() {
 		_t = 0f;
 		if(_expanding || _expanded) {
@@ -48,7 +51,8 @@ public class ActiveCustomGameController : MonoBehaviour {
 		}
 	}
 
-	// Update is called once per frame
+	// Runs every frame - stop at t = 1 because lerp is from 0 to 1
+	// Calculate deltaY to move all of the game prefabs under the one you click
 	void Update () {
 		if(_t >= 1f) {
 			_expanded = _expanding;
@@ -64,6 +68,8 @@ public class ActiveCustomGameController : MonoBehaviour {
 		}
 	}
 
+	// Moves all of the game prefabs under the one you clicked by using the sibling order
+	// also resize parent transform (scrollable area)
 	private void LerpBelowGames() {
 		int currIndex = transform.GetSiblingIndex();
 		int maxY = (transform.parent.childCount - 1) * -300;
