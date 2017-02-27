bfebb630df50da46bf7b4450f9f719dd2490dff3
Drew Kowalski <drewkowalski93@gmail.com>
2017-02-26 22:52:35 -0500

    trying to build the unity project in travis

--- TravisScripts/build.sh
+++ TravisScripts/build.sh
@@ -1,41 +1,37 @@
 #! /bin/sh
 
 # Change this the name of your project. This will be the name of the final executables as well.
-#project="Tactics"
+project="Tactics"
 
-#Test that the Django server is up and running
-sleep 5s
-curl -I http://127.0.0.1:8000
+echo "Attempting to build $project for Windows"
+/Applications/Unity/Unity.app/Contents/MacOS/Unity \
+  -batchmode \
+  -nographics \
+  -silent-crashes \
+  -logFile $(pwd)/unity.log \
+  -projectPath $(pwd) \
+  -buildWindowsPlayer "$(pwd)/Build/windows/$project.exe" \
+  -quit
 
-#echo "Attempting to build $project for Windows"
-#/Applications/Unity/Unity.app/Contents/MacOS/Unity \
-#  -batchmode \
-#  -nographics \
-#  -silent-crashes \
-#  -logFile $(pwd)/unity.log \
-#  -projectPath $(pwd) \
-#  -buildWindowsPlayer "$(pwd)/Build/windows/$project.exe" \
-#  -quit
+echo "Attempting to build $project for OS X"
+/Applications/Unity/Unity.app/Contents/MacOS/Unity \
+  -batchmode \
+  -nographics \
+  -silent-crashes \
+  -logFile $(pwd)/unity.log \
+  -projectPath $(pwd) \
+  -buildOSXUniversalPlayer "$(pwd)/Build/osx/$project.app" \
+  -quit
 
-#echo "Attempting to build $project for OS X"
-#/Applications/Unity/Unity.app/Contents/MacOS/Unity \
-#  -batchmode \
-#  -nographics \
-#  -silent-crashes \
-#  -logFile $(pwd)/unity.log \
-#  -projectPath $(pwd) \
-#  -buildOSXUniversalPlayer "$(pwd)/Build/osx/$project.app" \
-#  -quit
+echo "Attempting to build $project for Linux"
+/Applications/Unity/Unity.app/Contents/MacOS/Unity \
+  -batchmode \
+  -nographics \
+  -silent-crashes \
+  -logFile $(pwd)/unity.log \
+  -projectPath $(pwd) \
+  -buildLinuxUniversalPlayer "$(pwd)/Build/linux/$project.exe" \
+  -quit
 
-#echo "Attempting to build $project for Linux"
-#/Applications/Unity/Unity.app/Contents/MacOS/Unity \
-#  -batchmode \
-#  -nographics \
-#  -silent-crashes \
-#  -logFile $(pwd)/unity.log \
-#  -projectPath $(pwd) \
-#  -buildLinuxUniversalPlayer "$(pwd)/Build/linux/$project.exe" \
-#  -quit
-
-#echo 'Logs from build'
-#cat $(pwd)/unity.log
+echo 'Logs from build'
+cat $(pwd)/unity.log
--- TravisScripts/install.sh
+++ TravisScripts/install.sh
@@ -12,9 +12,8 @@ sudo ./Database/Create.sh tactics 12345
 python ./Server/manage.py makemigrations
 python ./Server/manage.py migrate
 
-#python ./Server/manage.py runserver
-#echo 'Downloading from http://netstorage.unity3d.com/unity/b7e030c65c9b/MacEditorInstaller/Unity-5.4.2f2.pkg: '
-#curl -o Unity.pkg http://netstorage.unity3d.com/unity/b7e030c65c9b/MacEditorInstaller/Unity-5.4.2f2.pkg
+echo 'Downloading from http://netstorage.unity3d.com/unity/b7e030c65c9b/MacEditorInstaller/Unity-5.4.2f2.pkg: '
+curl -o Unity.pkg http://netstorage.unity3d.com/unity/b7e030c65c9b/MacEditorInstaller/Unity-5.4.2f2.pkg
 
-#echo 'Installing Unity.pkg'
-#sudo installer -dumplog -package Unity.pkg -target /
+echo 'Installing Unity.pkg'
+sudo installer -dumplog -package Unity.pkg -target /
