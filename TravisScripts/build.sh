35d4de1b69ae7540747c586a1e2a55c0e86d03a4
Drew Kowalski <drewkowalski93@gmail.com>
2017-03-18 19:03:20 -0400

    only build android for now

--- TravisScripts/build.sh
+++ TravisScripts/build.sh
@@ -20,54 +20,7 @@ if [ "$rc" -eq "1" ]; then
 	exit $rc
 fi
 
-echo "Attempting to build $project for Windows"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity \
-  -batchmode \
-  -nographics \
-  -silent-crashes \
-  -logFile $(pwd)/unity.log \
-  -projectPath $(pwd) \
-  -buildWindowsPlayer "$(pwd)/Build/windows/$project.exe" \
-  -quit
-
-rc=$?
-if [ "$rc" -eq "1" ]; then
-	echo 'Logs from build'
-	cat $(pwd)/unity.log
-	exit $rc
-fi
-
-echo "Attempting to build $project for OS X"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity \
-  -batchmode \
-  -nographics \
-  -silent-crashes \
-  -logFile $(pwd)/unity.log \
-  -projectPath $(pwd) \
-  -buildOSXUniversalPlayer "$(pwd)/Build/osx/$project.app" \
-  -quit
-
-rc=$?
-if [ "$rc" -eq "1" ]; then
-	echo 'Logs from build'
-	cat $(pwd)/unity.log
-	exit $rc
-fi
-
-echo "Attempting to build $project for Linux"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity \
-  -batchmode \
-  -nographics \
-  -silent-crashes \
-  -logFile $(pwd)/unity.log \
-  -projectPath $(pwd) \
-  -buildLinuxUniversalPlayer "$(pwd)/Build/linux/$project.exe" \
-  -quit
-
 echo 'Logs from build'
 cat $(pwd)/unity.log
 
-rc=$?
-if [ "$rc" -eq "1" ]; then
-	exit $rc
-fi
+exit $rc
