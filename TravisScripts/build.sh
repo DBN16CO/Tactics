c49958fb63a49cd3ed9a611829bcf1730e339104
Drew Kowalski <drewkowalski93@gmail.com>
2017-03-18 18:06:10 -0400

    try building android on travis

--- TravisScripts/build.sh
+++ TravisScripts/build.sh
@@ -3,6 +3,23 @@
 # Change this the name of your project. This will be the name of the final executables as well.
 project="Tactics"
 
+echo "Attempting to build $project for Android"
+/Applications/Unity/Unity.app/Contents/MacOS/Unity \
+  -batchmode \
+  -nographics \
+  -silent-crashes \
+  -logFile $(pwd)/unity.log \
+  -projectPath $(pwd) \
+  -executeMethod "AutoBuilder.PerformAndroidBuild" \
+  -quit
+
+rc=$?
+if [ "$rc" -eq "1" ]; then
+	echo 'Logs from build'
+	cat $(pwd)/unity.log
+	exit $rc
+fi
+
 echo "Attempting to build $project for Windows"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
   -batchmode \
