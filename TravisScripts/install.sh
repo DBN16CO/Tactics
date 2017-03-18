7a832706b25ae2a7bbb5259d4207f44ef302aa5f
Drew Kowalski <drewkowalski93@gmail.com>
2017-03-18 18:29:31 -0400

    fix redis and add in installing unity

--- TravisScripts/install.sh
+++ TravisScripts/install.sh
@@ -11,3 +11,9 @@ sudo ./Database/Create.sh tactics 12345
 # Create the database tables for above database
 python ./Server/manage.py makemigrations
 python ./Server/manage.py migrate
+
+echo 'Downloading from http://netstorage.unity3d.com/unity/b7e030c65c9b/MacEditorInstaller/Unity-5.4.2f2.pkg: '
+curl -o Unity.pkg http://netstorage.unity3d.com/unity/b7e030c65c9b/MacEditorInstaller/Unity-5.4.2f2.pkg
+
+echo 'Installing Unity.pkg'
+sudo installer -dumplog -package Unity.pkg -target /
