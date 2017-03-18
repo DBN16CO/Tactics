5d72ab6c336edca4785d0ee7f34bd8a82d81c4b5
Drew Kowalski <drewkowalski93@gmail.com>
2017-03-18 17:17:43 -0400

    trying something different for builds

--- TravisScripts/build.sh
+++ TravisScripts/build.sh
@@ -1,56 +1,60 @@
-#! /bin/sh
+# #! /bin/sh
 
-# Change this the name of your project. This will be the name of the final executables as well.
-project="Tactics"
+xbuild /p:Configuration=Debug unity.sln
+unitysolution ./ iOS && xbuild /p:Configuration=Debug unity.sln
+unitysolution ./ Android && xbuild /p:Configuration=Debug unity.sln
 
-echo "Attempting to build $project for Windows"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity \
-  -batchmode \
-  -nographics \
-  -silent-crashes \
-  -logFile $(pwd)/unity.log \
-  -projectPath $(pwd) \
-  -buildWindowsPlayer "$(pwd)/Build/windows/$project.exe" \
-  -quit
+# # Change this the name of your project. This will be the name of the final executables as well.
+# project="Tactics"
 
-rc=$?
-if [ "$rc" -eq "1" ]; then
-	echo 'Logs from build'
-	cat $(pwd)/unity.log
-	exit $rc
-fi
+# echo "Attempting to build $project for Windows"
+# /Applications/Unity/Unity.app/Contents/MacOS/Unity \
+#   -batchmode \
+#   -nographics \
+#   -silent-crashes \
+#   -logFile $(pwd)/unity.log \
+#   -projectPath $(pwd) \
+#   -buildWindowsPlayer "$(pwd)/Build/windows/$project.exe" \
+#   -quit
 
-echo "Attempting to build $project for OS X"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity \
-  -batchmode \
-  -nographics \
-  -silent-crashes \
-  -logFile $(pwd)/unity.log \
-  -projectPath $(pwd) \
-  -buildOSXUniversalPlayer "$(pwd)/Build/osx/$project.app" \
-  -quit
+# rc=$?
+# if [ "$rc" -eq "1" ]; then
+# 	echo 'Logs from build'
+# 	cat $(pwd)/unity.log
+# 	exit $rc
+# fi
 
-rc=$?
-if [ "$rc" -eq "1" ]; then
-	echo 'Logs from build'
-	cat $(pwd)/unity.log
-	exit $rc
-fi
+# echo "Attempting to build $project for OS X"
+# /Applications/Unity/Unity.app/Contents/MacOS/Unity \
+#   -batchmode \
+#   -nographics \
+#   -silent-crashes \
+#   -logFile $(pwd)/unity.log \
+#   -projectPath $(pwd) \
+#   -buildOSXUniversalPlayer "$(pwd)/Build/osx/$project.app" \
+#   -quit
 
-echo "Attempting to build $project for Linux"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity \
-  -batchmode \
-  -nographics \
-  -silent-crashes \
-  -logFile $(pwd)/unity.log \
-  -projectPath $(pwd) \
-  -buildLinuxUniversalPlayer "$(pwd)/Build/linux/$project.exe" \
-  -quit
+# rc=$?
+# if [ "$rc" -eq "1" ]; then
+# 	echo 'Logs from build'
+# 	cat $(pwd)/unity.log
+# 	exit $rc
+# fi
 
-echo 'Logs from build'
-cat $(pwd)/unity.log
+# echo "Attempting to build $project for Linux"
+# /Applications/Unity/Unity.app/Contents/MacOS/Unity \
+#   -batchmode \
+#   -nographics \
+#   -silent-crashes \
+#   -logFile $(pwd)/unity.log \
+#   -projectPath $(pwd) \
+#   -buildLinuxUniversalPlayer "$(pwd)/Build/linux/$project.exe" \
+#   -quit
 
-rc=$?
-if [ "$rc" -eq "1" ]; then
-	exit $rc
-fi
+# echo 'Logs from build'
+# cat $(pwd)/unity.log
+
+# rc=$?
+# if [ "$rc" -eq "1" ]; then
+# 	exit $rc
+# fi
--- TravisScripts/install.sh
+++ TravisScripts/install.sh
@@ -12,8 +12,5 @@ sudo ./Database/Create.sh tactics 12345
 python ./Server/manage.py makemigrations
 python ./Server/manage.py migrate
 
-echo 'Downloading from http://netstorage.unity3d.com/unity/b7e030c65c9b/MacEditorInstaller/Unity-5.4.2f2.pkg: '
-curl -o Unity.pkg http://netstorage.unity3d.com/unity/b7e030c65c9b/MacEditorInstaller/Unity-5.4.2f2.pkg
-
-echo 'Installing Unity.pkg'
-sudo installer -dumplog -package Unity.pkg -target /
+npm install unity-solution-2 -g
+unitysolution ./ Standalone
