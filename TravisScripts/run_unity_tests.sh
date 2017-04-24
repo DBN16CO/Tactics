208fdaa9fc7ee024370cdfcc019f481b30454ed6
Brennan <bjg515@gmail.com>
2017-04-23 21:33:30 -0400

    Run unity headless

--- TravisScripts/run_unity_tests.sh
+++ TravisScripts/run_unity_tests.sh
@@ -1,6 +1,12 @@
 #! /bin/bash
-pwd
-/Applications/Unity/Unity.app/Contents/MacOS/Unity -batchmode -logFile -projectPath ./ -runEditorTests -quit
+/Applications/Unity/Unity.app/Contents/MacOS/Unity \
+  -batchmode \
+  -nographics \
+  -silent-crashes \
+  -logFile $(pwd)/unity_tests.log \
+  -projectPath $(pwd) \
+  -runEditorTests \
+  -quit
 result=$?
 echo "The Unity tests ran with an error code of $result"
 exit $result
