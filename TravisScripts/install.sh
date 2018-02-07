94cb5a60aa600bb6df22b7445c30746d4f054697
Brennan <bjg515@gmail.com>
2018-02-06 23:23:56 -0500

    Install pip manually now.

--- TravisScripts/install.sh
+++ TravisScripts/install.sh
@@ -3,6 +3,8 @@
 # This link changes from time to time. I haven't found a reliable hosted installer package for doing regular
 # installs like this. You will probably need to grab a current link from: http://unity3d.com/get-unity/download/archive
 echo 'Downloading Pre-reqs: '
+curl -O https://raw.github.com/pypa/pip/master/contrib/get-pip.py
+python get-pip.py
 pip install -r ./Server/requirements.txt
 
 # Setup PSQL and REDIS
