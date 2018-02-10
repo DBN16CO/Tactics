012d47a986f1d2c466ea71d73a4752d1a6622db7
Brennan <bjg515@gmail.com>
2018-02-09 22:14:29 -0500

    I give up, Drew fix it!

--- TravisScripts/install.sh
+++ TravisScripts/install.sh
@@ -3,9 +3,7 @@
 # This link changes from time to time. I haven't found a reliable hosted installer package for doing regular
 # installs like this. You will probably need to grab a current link from: http://unity3d.com/get-unity/download/archive
 echo 'Downloading Pre-reqs: '
-curl https://bootstrap.pypa.io/get-pip.py | sudo python
-sudo pip install pathlib
-sudo pip install -r ./Server/requirements.txt
+pip install -r ./Server/requirements.txt
 
 # Setup PSQL and REDIS
 export PG_DATA=$(brew --prefix)/var/postgres
