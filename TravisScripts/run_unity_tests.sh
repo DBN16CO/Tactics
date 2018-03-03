cdc75713cecc31ce40ac7c0091d89bf79f2c3dbc
Drew Kowalski <drewkowalski93@gmail.com>
2018-03-03 11:25:09 -0500

    Try changing the installer and also adding editor tests flag

--- TravisScripts/run_unity_tests.sh
+++ TravisScripts/run_unity_tests.sh
@@ -3,10 +3,20 @@
   -batchmode \
   -nographics \
   -silent-crashes \
-  -logFile $(pwd)/unity_tests.log \
+  -logFile $(pwd)/unity_logfile.log \
   -projectPath $(pwd) \
+  -editorTestsResultFile $(pwd)/unity_tests.log \
   -runEditorTests \
   -quit
 result=$?
 echo "The Unity tests ran with an exit code of $result"
+
+echo "-------------START unity_logfile.log-------------"
+cat $(pwd)/unity_logfile.log
+echo "-------------END unity_logfile.log-------------"
+
+echo "-------------START unity_tests.log-------------"
+cat $(pwd)/unity_tests.log
+echo "-------------END unity_tests.log-------------"
+
 exit $result
