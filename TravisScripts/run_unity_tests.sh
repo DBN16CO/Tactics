e1367e1ab30661bd8897f3fa5eb0f84576906d81
Drew Kowalski <drewkowalski93@gmail.com>
2018-03-03 11:44:13 -0500

    Try changing results file to xml file

--- TravisScripts/run_unity_tests.sh
+++ TravisScripts/run_unity_tests.sh
@@ -5,7 +5,7 @@
   -silent-crashes \
   -logFile $(pwd)/unity_logfile.log \
   -projectPath $(pwd) \
-  -editorTestsResultFile $(pwd)/unity_tests.log \
+  -editorTestsResultFile $(pwd)/unity_tests.xml \
   -runEditorTests \
   -quit
 result=$?
@@ -16,7 +16,7 @@ echo "The Unity tests ran with an exit code of $result"
 #echo "-------------END unity_logfile.log-------------"
 
 echo "-------------START unity_tests.log-------------"
-cat $(pwd)/unity_tests.log
+cat $(pwd)/unity_tests.xml
 echo "-------------END unity_tests.log-------------"
 
 exit $result
